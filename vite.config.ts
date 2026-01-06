8eb1a388b604b0538d8e8d9abecf3f4c32af5f90
Harshal Pohekar <132100605+harshya007@users.noreply.github.com>
2026-01-06 18:48:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/fashion-ai/", // 👈 repo name EXACT
+  base: "/fashion-ai/"
 });
