bd98206b3a3e8f85cad3653a0f4b4abdbe4aeb03
Harshal Pohekar <132100605+harshya007@users.noreply.github.com>
2026-01-06 18:41:57 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/fashion-ai/",
   server: {
     proxy: {
       "/api": "http://localhost:4000",
